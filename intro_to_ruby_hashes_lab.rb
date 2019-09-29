98b153b24597dd504784c688b1537e08fd553561
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-29 16:35:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def base_hash
-  {"Insane_clown_posse"} =>  members: "violent j", "shaggy 2 dope"
+  {"Insane_clown_posse"} =>  members: "violent j" "shaggy 2 dope"
 end
 
 def monopoly_with_second_tier
-  {"Insane_clown_posse"} =>  members: "violent j", "shaggy 2 dope"
+  {"Insane_clown_posse"} =>  members: "violent j" "shaggy 2 dope"
 end
 
 def monopoly_with_third_tier
