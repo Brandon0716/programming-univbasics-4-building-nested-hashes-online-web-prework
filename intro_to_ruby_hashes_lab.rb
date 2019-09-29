ff9b05a2c349cbe9c07096680a6c81073b7e817b
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-29 16:29:16 +0000

    Automatically backed up by Learn

---
+++ intro_to_ruby_hashes_lab.rb
@@ -0,0 +1,21 @@
+def base_hash
+  Insane_clown_posse =  members: "violent j", "shaggy 2 dope"
+end
+
+def monopoly_with_second_tier
+  Insane_clown_posse = members: "violent j", "shaggy 2 dope"
+end
+
+def monopoly_with_third_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+end
+
+def monopoly_with_fourth_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+end
