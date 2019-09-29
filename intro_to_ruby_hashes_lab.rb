605f4415f3c5d898189b9853621c6873bbad1b99
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-29 16:44:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,9 @@ def base_hash
 end
 
 def monopoly_with_second_tier
- 
+ flatiron_school = {
+  instructors: ["Avi", "Jeff", "Rose"], 
+  dev_team: ["Jonas", "Logan", "Amanda", "Seiji", "Kate", "Spencer"]
 end
 
 def monopoly_with_third_tier
